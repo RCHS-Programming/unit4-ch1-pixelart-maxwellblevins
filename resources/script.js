3220a953f5419f928995f41233b33d7187367989
Maxwell Blevins <57766761+maxwellblevins@users.noreply.github.com>
2019-11-18 12:26:01 -0500

    11/18/2019
    added clicking different colours

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+function setPixelColour(pixel) 
+{ 
+  pixel.style.backgroundColor = 'black';
+}var penColour = 'black'; 
+
+function setPenColour(pen)
+{ 
+    penColour = pen;
+} 
+
+function setPixelColour(pixel) 
+{ 
+    pixel.style.backgroundColor = penColour;
+}
